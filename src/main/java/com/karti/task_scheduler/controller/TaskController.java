ea7ee585d4afc007afafc25b42d3ea6762d48b97
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-20 04:42:29 +0530

    feat: configured task controller to create tasks

--- src/main/java/com/karti/task_scheduler/controller/TaskController.java
+++ src/main/java/com/karti/task_scheduler/controller/TaskController.java
@@ -1,10 +1,25 @@
 package com.karti.task_scheduler.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.karti.task_scheduler.entity.Task;
+import com.karti.task_scheduler.service.TaskService;
 
 @RestController
 public class TaskController {
     
-    
+    private final TaskService service;
+
+    public TaskController(TaskService service){
+        this.service=service;
+    }
+
+    public ResponseEntity<?> saveTask(@RequestBody Task task){
+        service.saveTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
 }
