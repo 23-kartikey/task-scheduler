115425b3d5e646788967a648319b29915ab3871e
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-20 04:38:46 +0530

    feat: configured Task service class to save tasks

--- src/main/java/com/karti/task_scheduler/service/TaskService.java
+++ src/main/java/com/karti/task_scheduler/service/TaskService.java
@@ -2,9 +2,20 @@ package com.karti.task_scheduler.service;
 
 import org.springframework.stereotype.Service;
 
+import com.karti.task_scheduler.entity.Task;
+import com.karti.task_scheduler.repository.TaskRepository;
+
 @Service
 public class TaskService {
     
-    
+    private final TaskRepository repo;
+
+    public TaskService(TaskRepository repo){
+        this.repo=repo;
+    }
+
+    public void saveTask(Task task){
+        repo.save(task);
+    }
 
 }
