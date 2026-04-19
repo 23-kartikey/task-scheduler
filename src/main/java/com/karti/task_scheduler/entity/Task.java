a11f979a334d490204ffc0e396e8e3cd3e70fafb
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-20 04:28:52 +0530

    feat: added validation and data annotations to task attributes

--- src/main/java/com/karti/task_scheduler/entity/Task.java
+++ src/main/java/com/karti/task_scheduler/entity/Task.java
@@ -2,7 +2,13 @@ package com.karti.task_scheduler.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,21 +26,30 @@ import lombok.Setter;
 @Builder
 public class Task{
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
 
+    @Column(nullable=false)
     private String title;
 
+    @Column(length=1000)
     private String description;
 
+    @Column(nullable=false, updatable=false)
     private LocalDateTime createdAt;
 
+    @Column(nullable=false)
     private LocalDateTime reminderTime;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable=false)
     private Status status;
 
     private LocalDateTime lastAttemptTime;
 
-    private Integer retryCount;
+    @Column(nullable=false)
+    private Integer retryCount = 0 ;
 
     @PrePersist
     public void prePersist(){
