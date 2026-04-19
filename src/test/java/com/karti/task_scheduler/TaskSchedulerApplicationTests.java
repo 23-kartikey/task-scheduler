837efe32520ba0db6def832a770f5dde4fc7438b
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-20 03:56:47 +0530

    initial commit: setup files

---
+++ src/test/java/com/karti/task_scheduler/TaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karti.task_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
