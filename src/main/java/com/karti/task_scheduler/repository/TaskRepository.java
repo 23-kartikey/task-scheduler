db14088781c27c4c8d8015bc7944d7e4c7d5f35f
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-20 08:44:06 +0530

    feat: implemented a method to fetch incomplete tasks beyong reminder time

--- src/main/java/com/karti/task_scheduler/repository/TaskRepository.java
+++ src/main/java/com/karti/task_scheduler/repository/TaskRepository.java
@@ -1,9 +1,15 @@
 package com.karti.task_scheduler.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.karti.task_scheduler.entity.Status;
 import com.karti.task_scheduler.entity.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long>{
     
+    public List<Task> findByReminderTimeBeforeAndStatusNot(LocalDateTime now, Status status);
+
 }
