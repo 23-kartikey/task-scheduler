a781deb4a68762048f42f9023555ebf4b40c8718
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-23 07:51:25 +0530

    feat: added method implementation to fetch tasks with respective id and PENDING status

--- src/main/java/com/karti/task_scheduler/repository/TaskRepository.java
+++ src/main/java/com/karti/task_scheduler/repository/TaskRepository.java
@@ -2,6 +2,7 @@ package com.karti.task_scheduler.repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,4 +13,7 @@ public interface TaskRepository extends JpaRepository<Task, Long>{
     
     public List<Task> findByReminderTimeBeforeAndStatusNot(LocalDateTime now, Status status);
 
+    public Optional<Task> findByIdAndStatusIs(Long id, Status status);
+
+    public List<Task> findByRetryCountGreaterThanAndStatusNot(int count, Status status);
 }
