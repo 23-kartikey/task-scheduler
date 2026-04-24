b3915f4029fd8024362b9739302798e5bf4e8460
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-24 19:58:04 +0530

    refactor: removed scheduler annotation

--- src/main/java/com/karti/task_scheduler/TaskSchedulerApplication.java
+++ src/main/java/com/karti/task_scheduler/TaskSchedulerApplication.java
@@ -2,6 +2,7 @@ package com.karti.task_scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class TaskSchedulerApplication {
