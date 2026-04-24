d1fe5ad6493813027d375ccb9638cef7103d1a04
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-24 20:06:15 +0530

    feat: schedule method to log pending tasks

--- src/main/java/com/karti/task_scheduler/scheduler/ReminderScheduler.java
+++ src/main/java/com/karti/task_scheduler/scheduler/ReminderScheduler.java
@@ -1,18 +1,17 @@
 package com.karti.task_scheduler.scheduler;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.karti.task_scheduler.entity.Status;
 import com.karti.task_scheduler.entity.Task;
 import com.karti.task_scheduler.repository.TaskRepository;
-import jakarta.annotation.PostConstruct;
 
 @Component
 public class ReminderScheduler {
@@ -26,13 +25,15 @@ public class ReminderScheduler {
         this.repo=repo;
     }
 
-    @PostConstruct
-    public void reminderScheduler(){
-        scheduler.scheduleAtFixedRate(()->checkTasks(), Duration.ofSeconds(5));
-    }
+    // @PostConstruct
+    // public void reminderScheduler(){
+    //     scheduler.scheduleAtFixedRate(()->checkTasks(), Duration.ofSeconds(10));
+    // }
 
-    public void checkTasks(){
-        List<Task> tasks=repo.findByReminderTimeBeforeAndStatusNot(LocalDateTime.now(), Status.COMPLETED);
+    @Scheduled(fixedRate=10000)
+    public void checkTasks() throws InterruptedException{
+        logger.info("LOGGING INCOMPLETE TASKS:-->");
+        List<Task> tasks=repo.findByReminderTimeBeforeAndStatusIs(LocalDateTime.now(), Status.PENDING);
         for(Task task: tasks){
             logger.info("Incomplete: "+task.getId()+" "+task.getTitle()+", "+task.getRetryCount()+" "+task.getStatus().toString());
         }
