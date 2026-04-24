255dac079a718e3d19127294b4a587a630a9b47e
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-24 20:12:03 +0530

    feat: create Bot and User class as child of Creator as the two task creating entities

---
+++ src/main/java/com/karti/task_scheduler/entity/Bot.java
@@ -0,0 +1,19 @@
+package com.karti.task_scheduler.entity;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@DiscriminatorValue("BOT")
+public class Bot extends Creator{
+
+    private String name;
+
+}
---
+++ src/main/java/com/karti/task_scheduler/entity/User.java
@@ -0,0 +1,27 @@
+package com.karti.task_scheduler.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@DiscriminatorValue("USER")
+public class User extends Creator{
+
+    @Column(unique=true)
+    private String username;
+
+
+}
